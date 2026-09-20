28f2d2966bca8b1167f47f1405584642e00dc8d4
Michał Wrona <wronamichal01@gmail.com>
2026-09-20 18:14:09 +0200

    Update

--- src/app/schemas.py
+++ src/app/schemas.py
@@ -1,19 +1,25 @@
 from datetime import date
 from typing import Any, Dict
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class ScheduleCreate(BaseModel):
     date: date
     day_of_week: str
     group: str
-    section: str
+    section: str | int
     mode: str
     hours: Dict[str, Any] = {}
 
     class Config:
         from_attributes = True
+
+    @field_validator("section", mode="before")
+    @classmethod
+    def convert_section_to_str(cls, value):
+        return str(value)
+
 
 
 class ScheduleResponse(ScheduleCreate):
